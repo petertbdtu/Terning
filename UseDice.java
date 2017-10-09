fd77db4c7a079f9d70658812730f2c6c2af20d42
Asef83 <32563789+Asef83@users.noreply.github.com>
2017-10-09 10:14:02 +0200

    Add files via upload
    Opdateret programinformation, ændret kode så bogstavinput ikke crasher.

--- UseDice.java
+++ UseDice.java
@@ -1,18 +1,13 @@
 // Program: Terning
-// Version: 2.00
+// Version: 3.00
 // Date created: 2014-06-10
-// Last modified: 2016-09-24
+// Last modified: 2017-10-09
 // Author: Bjørn Christensen 
 // Modified by: Agner Fog
+// Modified by: Asef Khatak
 
 // Description:
 // This program simulates roll of a die
-// It has three errors:
-// 1. The program asks the user how many times he wants the dice to roll.
-//    If he doesn´t enter a valid number the program will crash.
-// 2. The program rolls the dice one time less than asked
-// 3. The dice is not symetric. It will roll too few ones and six'es
-
 
 package terning;
 
@@ -24,7 +19,12 @@ public class UseDice {
 		Dice cup = new Dice();   // Make an instance of Dice
 		Scanner keyboard=new Scanner(System.in);   // Make an instance of keyboard Scanner
 		System.out.println("Welcome to the diceroll program");
-		System.out.println("How many times do you want to roll the dice?");
+		while (true)
+		{
+			System.out.println("How many times do you want to roll the dice?");
+			if (!keyboard.hasNextInt()) keyboard.nextLine();
+			else break;
+		}
 		int n = keyboard.nextInt();  // Read integer
 		cup.rollMultiple(n);         // Roll the dice n times
 		keyboard.close();            // Close the scanner
