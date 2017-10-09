de970700206bafae2f8d3d0d32603cf5ef23a53c
SebastianStokkebro <32563781+SebastianStokkebro@users.noreply.github.com>
2017-10-09 09:57:42 +0200

    Add files via upload
    terningslag simplificeret --- for løkke iterationsstartværdi ændret fra 1 til 0.

--- Dice.java
+++ Dice.java
@@ -1,4 +1,17 @@
+package terning;
 
 public class Dice {
+	// roll the die and return a value between 1-6
+	public int roll() {
+		int value = (int)(Math.random() * 6.0) + 1;
+	}
+
+	// roll the die n times and print the values
+	public void rollMultiple(int n) {
+		for (int i=0; i<n; i++) {
+			System.out.print(roll() + " ");
+		}		
+
+	}
 
 }
